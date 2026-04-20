546cf9c80792036b80323cb560b23d4acbbf7f6b
andrzej.witkowski <andrzej.witkowski@allegro.com>
2026-04-20 21:28:55 +0200

    test: stop retaining frontend fixtures across suites

--- scripts/verify_rust_tests.sh
+++ scripts/verify_rust_tests.sh
@@ -2,41 +2,4 @@
 
 set -eu
 
-cargo test --lib
-
-for target in \
-  athlete_summary_service \
-  auth_rest \
-  calendar_entry_views_mongo \
-  canonical_roots_mongo \
-  dashboard_rest \
-  external_sync_mongo \
-  health_check \
-  identity_domain \
-  identity_service \
-  intervals_adapters \
-  intervals_pest_parser_poc \
-  intervals_planned_workout \
-  intervals_rest \
-  intervals_service \
-  intervals_workout_analysis \
-  llm_adapters \
-  llm_rest \
-  logs_rest \
-  main_runtime \
-  races_mongo \
-  settings \
-  settings_rest \
-  task_scheduler \
-  telemetry_setup \
-  training_load_mongo \
-  training_plan_mongo \
-  training_plan_service \
-  workout_summary_mongo \
-  workout_summary_rest \
-  workout_summary_service
-do
-  cargo test --test "$target"
-done
-
-cargo test --bin aiwattcoach
+cargo test -- --nocapture
